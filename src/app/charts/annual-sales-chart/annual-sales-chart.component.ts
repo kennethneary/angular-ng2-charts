fa6634a490030e2baaf55ea7a63cd9745bd68492
Kenneth Neary <kennethneary@gmail.com>
2020-08-03 17:07:47 +0100

    ng2 charts, ng material

---
+++ src/app/charts/annual-sales-chart/annual-sales-chart.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import { SalesService } from 'src/app/sales/sales.service';
+
+@Component({
+  selector: 'app-annual-sales-chart',
+  templateUrl: './annual-sales-chart.component.html',
+  styleUrls: ['./annual-sales-chart.component.scss']
+})
+export class AnnualSalesChartComponent implements OnInit {
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Total Sales' },
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType: ChartType = 'line';
+  public lineChartPlugins = [];
+
+  public salesChartLabels: Label[] = [];
+
+  constructor(private salesService: SalesService) { }
+
+  ngOnInit() {
+  }
+
+}
